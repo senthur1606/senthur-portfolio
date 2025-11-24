018dc2e4508e435b9226e632275dff6bf0c971c6
Senthur pandian <skpandian8@gmail.com>
2025-11-24 07:54:12 +0530

    initial portfolio upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Set current year in footer
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
